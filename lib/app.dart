69ed69fd17a737291db2749abcdb01905cdfb4e1
morigu812-svg <morigu812@gmail.com>
2026-08-02 18:55:44 +0900

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'features/auth/login_page.dart';
 
+class StudyAIApp extends StatelessWidget {
+  const StudyAIApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "StudyAI",
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      home: const LoginPage(),
+    );
+  }
+}
